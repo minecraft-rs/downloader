19e7f09f6c6d82dcea52b59050949e892fc5fe4b
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2023-01-27 00:13:06 -0400

    feat: use result if new function

--- examples/download_version.rs
+++ examples/download_version.rs
@@ -1,8 +1,12 @@
 use mc_downloader::ClientDownloader;
 
 fn main() {
-    let downloader: ClientDownloader = ClientDownloader::new();
-    downloader
-        .download_version("1.19.3".to_string(), "%appdata%\\.minecraft".to_string())
-        .unwrap();
+    match ClientDownloader::new() {
+        Ok(downloader) => {
+            downloader
+                .download_version("1.19.3".to_string(), "%appdata%\\.minecraft".to_string())
+                .unwrap();
+        }
+        Err(e) => println!("{e:?}"),
+    }
 }
