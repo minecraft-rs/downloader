0058f37c6ae9c15453f1d369150e4bce8ea040ea
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2023-02-05 01:47:13 -0400

    refactor: convert version type into a enum and add tests and to_string methods for enum

--- src/manifest.rs
+++ src/manifest.rs
@@ -61,6 +61,15 @@ pub struct ManifestLibrary {
     pub rules: Option<Vec<ManifestRule>>,
 }
 
+#[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Eq)]
+#[serde(rename_all = "snake_case")]
+pub enum VersionType {
+    Release,
+    Snapshot,
+    OldBeta,
+    OldAlpha,
+}
+
 #[derive(Clone, Serialize, Deserialize)]
 #[serde(rename_all(deserialize = "camelCase"))]
 pub struct Manifest {
@@ -75,8 +84,8 @@ pub struct Manifest {
     pub minimum_launcher_version: i8,
     pub release_time: String,
     pub time: String,
-    #[serde(rename(deserialize = "type"))]
-    pub type_: String,
+    #[serde(rename = "type")]
+    pub type_: VersionType,
 }
 
 pub fn read_manifest_from_str(string: &str) -> Result<Manifest, ManifestError> {
@@ -88,4 +97,64 @@ pub fn read_manifest_from_file(file: &str) -> Result<Manifest, ManifestError> {
     let raw = fs::read_to_string(file)?;
     let manifest: Manifest = read_manifest_from_str(&raw)?;
     Ok(manifest)
+}
+
+impl ToString for VersionType {
+    fn to_string(&self) -> String {
+        match *self {
+            VersionType::Release => String::from("Release"),
+            VersionType::Snapshot => String::from("Snapshot"),
+            VersionType::OldAlpha | VersionType::OldBeta => String::from("Old"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use serde::{Deserialize, Serialize};
+
+    use super::VersionType;
+
+    #[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+    #[serde(rename_all(deserialize = "camelCase"))]
+    struct TestStruct {
+        #[serde(rename = "type")]
+        type_: VersionType,
+    }
+
+    #[test]
+    fn version_type_serialize() {
+        let st = TestStruct {
+            type_: VersionType::Release,
+        };
+        let expected_json = r#"{"type":"release"}"#;
+        let json = serde_json::to_string(&st);
+
+        assert!(json.is_ok());
+        assert_eq!(json.unwrap(), expected_json);
+    }
+
+    #[test]
+    fn version_type_serialize_snake_case() {
+        let st = TestStruct {
+            type_: VersionType::OldAlpha,
+        };
+        let expected_json = r#"{"type":"old_alpha"}"#;
+        let json = serde_json::to_string(&st);
+
+        assert!(json.is_ok());
+        assert_eq!(json.unwrap(), expected_json);
+    }
+
+    #[test]
+    fn version_type_deserialize() {
+        let raw_json = r#"{"type":"old_beta"}"#;
+        let expected_st = TestStruct {
+            type_: VersionType::OldBeta,
+        };
+        let json = serde_json::from_str::<TestStruct>(raw_json);
+
+        assert!(json.is_ok());
+        assert_eq!(json.unwrap(), expected_st);
+    }
 }
